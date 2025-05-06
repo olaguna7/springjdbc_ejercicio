bfa4ccd3afd311673dced4eb44e05cfe9145d4b5
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-06 11:59:28 +0200

    Second commit

---
+++ src/main/java/com/oscar/springjdbc_ejercicio/controladores/ClienteController.java
@@ -0,0 +1,22 @@
+package com.oscar.springjdbc_ejercicio.controladores;
+
+import com.oscar.springjdbc_ejercicio.servicios.ClienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping("/clientes")
+    public String listaClientes(Model model) {
+        model.addAttribute("clientes", clienteService.listarClientes());
+        return "listaClientes";
+    }
+}
--- src/main/java/com/oscar/springjdbc_ejercicio/servicios/ClienteService.java
+++ src/main/java/com/oscar/springjdbc_ejercicio/servicios/ClienteService.java
@@ -1,8 +1,19 @@
 package com.oscar.springjdbc_ejercicio.servicios;
 
+import com.oscar.springjdbc_ejercicio.entidades.Cliente;
+import com.oscar.springjdbc_ejercicio.repositorios.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Iterable<Cliente> listarClientes() {
+
+    }
 }
